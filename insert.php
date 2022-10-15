d6962d2ff11e49c0ebb1d824ef71d158131d1e96
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2022-10-15 21:42:20 +0300

    backend

---
+++ insert.php
@@ -0,0 +1,24 @@
+<?php
+require 'connect.php';
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header('Acecess-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata)) {
+    $request = json_decode($postdata);
+
+    $name = $request->name;
+    $email = $request->email;
+    $phone = $request->phone;
+    $password = $request->password;
+    $sql = "INSERT INTO users (name,email,phone,password) VALUES ('$name','$email','$phone','$password')";
+    if(mysqli_query($db,$sql)){
+        http_response_code(201);
+    }
+    else{
+         http_response_code(422); 
+    }
+         
+}
+?>
